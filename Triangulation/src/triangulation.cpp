bdc777159c9ae742bce2943358a913ec5021fb2d
Dong-Won Shin <dongwon.shin@stradvision.com>
2022-07-11 00:07:04 +0900

    feat: refactor triangulation module

--- Triangulation/src/triangulation.cpp
+++ Triangulation/src/triangulation.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-// #include "extra.h" // used in opencv2
+
 using namespace std;
 using namespace cv;
 
-void find_feature_matches(
-  const Mat &img_1, const Mat &img_2,
-  std::vector<KeyPoint> &keypoints_1,
-  std::vector<KeyPoint> &keypoints_2,
-  std::vector<DMatch> &matches);
+void find_feature_matches(const Mat &img_1, const Mat &img_2,
+                          std::vector<KeyPoint> &keypoints_1,
+                          std::vector<KeyPoint> &keypoints_2,
+                          std::vector<DMatch> &matches);
 
-void pose_estimation_2d2d(
-  const std::vector<KeyPoint> &keypoints_1,
-  const std::vector<KeyPoint> &keypoints_2,
-  const std::vector<DMatch> &matches,
-  Mat &R, Mat &t);
+void pose_estimation_2d2d(const std::vector<KeyPoint> &keypoints_1,
+                          const std::vector<KeyPoint> &keypoints_2,
+                          const std::vector<DMatch> &matches, Mat &R, Mat &t);
 
-void triangulation(
-  const vector<KeyPoint> &keypoint_1,
-  const vector<KeyPoint> &keypoint_2,
-  const std::vector<DMatch> &matches,
-  const Mat &R, const Mat &t,
-  vector<Point3d> &points
-);
+void triangulation(const vector<KeyPoint> &keypoint_1,
+                   const vector<KeyPoint> &keypoint_2,
+                   const std::vector<DMatch> &matches, const Mat &R,
+                   const Mat &t, vector<Point3d> &points);
 
 inline cv::Scalar get_color(float depth) {
   float up_th = 50, low_th = 10, th_range = up_th - low_th;
-  if (depth > up_th) depth = up_th;
-  if (depth < low_th) depth = low_th;
+  if (depth > up_th)
+    depth = up_th;
+  if (depth < low_th)
+    depth = low_th;
   return cv::Scalar(255 * depth / th_range, 0, 255 * (1 - depth / th_range));
 }
 
@@ -58,11 +54,14 @@ int main(int argc, char **argv) {
     float depth1 = points[i].z;
     cout << "depth: " << depth1 << endl;
     Point2d pt1_cam = pixel2cam(keypoints_1[matches[i].queryIdx].pt, K);
-    cv::circle(img1_plot, keypoints_1[matches[i].queryIdx].pt, 2, get_color(depth1), 2);
+    cv::circle(img1_plot, keypoints_1[matches[i].queryIdx].pt, 2,
+               get_color(depth1), 2);
 
-    Mat pt2_trans = R * (Mat_<double>(3, 1) << points[i].x, points[i].y, points[i].z) + t;
+    Mat pt2_trans =
+        R * (Mat_<double>(3, 1) << points[i].x, points[i].y, points[i].z) + t;
     float depth2 = pt2_trans.at<double>(2, 0);
-    cv::circle(img2_plot, keypoints_2[matches[i].trainIdx].pt, 2, get_color(depth2), 2);
+    cv::circle(img2_plot, keypoints_2[matches[i].trainIdx].pt, 2,
+               get_color(depth2), 2);
   }
   cv::imshow("img 1", img1_plot);
   cv::imshow("img 2", img2_plot);
@@ -81,8 +80,10 @@ void find_feature_matches(const Mat &img_1, const Mat &img_2,
   Ptr<DescriptorExtractor> descriptor = ORB::create();
   // use this if you are in OpenCV2
   // Ptr<FeatureDetector> detector = FeatureDetector::create ( "ORB" );
-  // Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ( "ORB" );
-  Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+  // Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ( "ORB"
+  // );
+  Ptr<DescriptorMatcher> matcher =
+      DescriptorMatcher::create("BruteForce-Hamming");
   detector->detect(img_1, keypoints_1);
   detector->detect(img_2, keypoints_2);
 
@@ -97,8 +98,10 @@ void find_feature_matches(const Mat &img_1, const Mat &img_2,
 
   for (int i = 0; i < descriptors_1.rows; i++) {
     double dist = match[i].distance;
-    if (dist < min_dist) min_dist = dist;
-    if (dist > max_dist) max_dist = dist;
+    if (dist < min_dist)
+      min_dist = dist;
+    if (dist > max_dist)
+      max_dist = dist;
   }
 
   printf("-- Max dist : %f \n", max_dist);
@@ -111,17 +114,15 @@ void find_feature_matches(const Mat &img_1, const Mat &img_2,
   }
 }
 
-void pose_estimation_2d2d(
-  const std::vector<KeyPoint> &keypoints_1,
-  const std::vector<KeyPoint> &keypoints_2,
-  const std::vector<DMatch> &matches,
-  Mat &R, Mat &t) {
+void pose_estimation_2d2d(const std::vector<KeyPoint> &keypoints_1,
+                          const std::vector<KeyPoint> &keypoints_2,
+                          const std::vector<DMatch> &matches, Mat &R, Mat &t) {
   Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
 
   vector<Point2f> points1;
   vector<Point2f> points2;
 
-  for (int i = 0; i < (int) matches.size(); i++) {
+  for (int i = 0; i < (int)matches.size(); i++) {
     points1.push_back(keypoints_1[matches[i].queryIdx].pt);
     points2.push_back(keypoints_2[matches[i].trainIdx].pt);
   }
@@ -129,30 +130,27 @@ void pose_estimation_2d2d(
   Point2d principal_point(325.1, 249.7);
   int focal_length = 521;
   Mat essential_matrix;
-  essential_matrix = findEssentialMat(points1, points2, focal_length, principal_point);
+  essential_matrix =
+      findEssentialMat(points1, points2, focal_length, principal_point);
 
-  recoverPose(essential_matrix, points1, points2, R, t, focal_length, principal_point);
+  recoverPose(essential_matrix, points1, points2, R, t, focal_length,
+              principal_point);
 }
 
-void triangulation(
-  const vector<KeyPoint> &keypoint_1,
-  const vector<KeyPoint> &keypoint_2,
-  const std::vector<DMatch> &matches,
-  const Mat &R, const Mat &t,
-  vector<Point3d> &points) {
-  Mat T1 = (Mat_<float>(3, 4) <<
-    1, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0);
-  Mat T2 = (Mat_<float>(3, 4) <<
-    R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2), t.at<double>(0, 0),
-    R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2), t.at<double>(1, 0),
-    R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2), t.at<double>(2, 0)
-  );
+void triangulation(const vector<KeyPoint> &keypoint_1,
+                   const vector<KeyPoint> &keypoint_2,
+                   const std::vector<DMatch> &matches, const Mat &R,
+                   const Mat &t, vector<Point3d> &points) {
+  Mat T1 = (Mat_<float>(3, 4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0);
+  Mat T2 = (Mat_<float>(3, 4) << R.at<double>(0, 0), R.at<double>(0, 1),
+            R.at<double>(0, 2), t.at<double>(0, 0), R.at<double>(1, 0),
+            R.at<double>(1, 1), R.at<double>(1, 2), t.at<double>(1, 0),
+            R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2),
+            t.at<double>(2, 0));
 
   Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
   vector<Point2f> pts_1, pts_2;
-  for (DMatch m:matches) {
+  for (DMatch m : matches) {
     pts_1.push_back(pixel2cam(keypoint_1[m.queryIdx].pt, K));
     pts_2.push_back(pixel2cam(keypoint_2[m.trainIdx].pt, K));
   }
@@ -163,20 +161,12 @@ void triangulation(
   for (int i = 0; i < pts_4d.cols; i++) {
     Mat x = pts_4d.col(i);
     x /= x.at<float>(3, 0);
-    Point3d p(
-      x.at<float>(0, 0),
-      x.at<float>(1, 0),
-      x.at<float>(2, 0)
-    );
+    Point3d p(x.at<float>(0, 0), x.at<float>(1, 0), x.at<float>(2, 0));
     points.push_back(p);
   }
 }
 
 Point2f pixel2cam(const Point2d &p, const Mat &K) {
-  return Point2f
-    (
-      (p.x - K.at<double>(0, 2)) / K.at<double>(0, 0),
-      (p.y - K.at<double>(1, 2)) / K.at<double>(1, 1)
-    );
+  return Point2f((p.x - K.at<double>(0, 2)) / K.at<double>(0, 0),
+                 (p.y - K.at<double>(1, 2)) / K.at<double>(1, 1));
 }
-
