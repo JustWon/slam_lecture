9820655014e3c97daa93e14bf28f5a711137433b
Dong-Won Shin <dongwon.shin@stradvision.com>
2022-07-11 00:12:53 +0900

    feat: add files for 3D Point Camera Projection

---
+++ 3DPointCameraProjection/src/struct.hpp
@@ -0,0 +1,12 @@
+#ifndef STRUCT_HPP
+#define STRUCT_HPP
+
+struct LidarPoint { // single lidar point in space
+    double x,y,z,r; // x,y,z in [m], r is point reflectivity
+};
+
+#endif // STRUCT_HPP
+
+
+
+
