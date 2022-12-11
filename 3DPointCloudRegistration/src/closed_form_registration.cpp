e53f84f5392a9b93855ee6e9e9f1251e01ad3440
Dong-Won Shin <dongwon.shin@stradvision.com>
2022-12-11 04:09:38 +0000

    update 3DPointCloudRegistration

--- 3DPointCloudRegistration/src/closed_form_registration.cpp
+++ 3DPointCloudRegistration/src/closed_form_registration.cpp
@@ -3,12 +3,6 @@
 #include <Eigen/Geometry>
 #include <Eigen/SVD>
 #include <chrono>
-#include <g2o/core/base_unary_edge.h>
-#include <g2o/core/base_vertex.h>
-#include <g2o/core/block_solver.h>
-#include <g2o/core/optimization_algorithm_gauss_newton.h>
-#include <g2o/core/optimization_algorithm_levenberg.h>
-#include <g2o/solvers/dense/linear_solver_dense.h>
 #include <iostream>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/core/core.hpp>
@@ -28,58 +22,6 @@ Point2d pixel2cam(const Point2d &p, const Mat &K);
 
 void pose_estimation_3d3d(const vector<Point3f> &pts1,
                           const vector<Point3f> &pts2, Mat &R, Mat &t);
-
-void bundle_adjustment(const vector<Point3f> &points_3d,
-                       const vector<Point3f> &points_2d, Mat &R, Mat &t);
-
-/// vertex and edges used in g2o ba
-class VertexPose : public g2o::BaseVertex<6, Sophus::SE3d> {
-public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-
-  virtual void setToOriginImpl() override { _estimate = Sophus::SE3d(); }
-
-  /// left multiplication on SE3
-  virtual void oplusImpl(const double *update) override {
-    Eigen::Matrix<double, 6, 1> update_eigen;
-    update_eigen << update[0], update[1], update[2], update[3], update[4],
-        update[5];
-    _estimate = Sophus::SE3d::exp(update_eigen) * _estimate;
-  }
-
-  virtual bool read(istream &in) override {}
-
-  virtual bool write(ostream &out) const override {}
-};
-
-/// g2o edge
-class EdgeProjectXYZRGBDPoseOnly
-    : public g2o::BaseUnaryEdge<3, Eigen::Vector3d, VertexPose> {
-public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-
-  EdgeProjectXYZRGBDPoseOnly(const Eigen::Vector3d &point) : _point(point) {}
-
-  virtual void computeError() override {
-    const VertexPose *pose = static_cast<const VertexPose *>(_vertices[0]);
-    _error = _measurement - pose->estimate() * _point;
-  }
-
-  virtual void linearizeOplus() override {
-    VertexPose *pose = static_cast<VertexPose *>(_vertices[0]);
-    Sophus::SE3d T = pose->estimate();
-    Eigen::Vector3d xyz_trans = T * _point;
-    _jacobianOplusXi.block<3, 3>(0, 0) = -Eigen::Matrix3d::Identity();
-    _jacobianOplusXi.block<3, 3>(0, 3) = Sophus::SO3d::hat(xyz_trans);
-  }
-
-  bool read(istream &in) {}
-
-  bool write(ostream &out) const {}
-
-protected:
-  Eigen::Vector3d _point;
-};
 
 int main(int argc, char **argv) {
   if (argc != 5) {
@@ -122,19 +64,6 @@ int main(int argc, char **argv) {
   cout << "R_inv = " << R.t() << endl;
   cout << "t_inv = " << -R.t() * t << endl;
 
-  cout << "calling bundle adjustment" << endl;
-
-  bundle_adjustment(pts1, pts2, R, t);
-
-  // verify p1 = R * p2 + t
-  for (int i = 0; i < 5; i++) {
-    cout << "p1 = " << pts1[i] << endl;
-    cout << "p2 = " << pts2[i] << endl;
-    cout << "(R*p2+t) = "
-         << R * (Mat_<double>(3, 1) << pts2[i].x, pts2[i].y, pts2[i].z) + t
-         << endl;
-    cout << endl;
-  }
 }
 
 void find_feature_matches(const Mat &img_1, const Mat &img_2,
@@ -232,50 +161,3 @@ void pose_estimation_3d3d(const vector<Point3f> &pts1,
   t = (Mat_<double>(3, 1) << t_(0, 0), t_(1, 0), t_(2, 0));
 }
 
-void bundle_adjustment(const vector<Point3f> &pts1, const vector<Point3f> &pts2,
-                       Mat &R, Mat &t) {
-  typedef g2o::BlockSolverX BlockSolverType;
-  typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType>
-      LinearSolverType;
-  auto solver = new g2o::OptimizationAlgorithmLevenberg(
-      g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
-  g2o::SparseOptimizer optimizer;
-  optimizer.setAlgorithm(solver);
-  optimizer.setVerbose(true);
-  optimizer.setVerbose(true);
-
-  // vertex
-  VertexPose *pose = new VertexPose(); // camera pose
-  pose->setId(0);
-  pose->setEstimate(Sophus::SE3d());
-  optimizer.addVertex(pose);
-
-  // edges
-  for (size_t i = 0; i < pts1.size(); i++) {
-    EdgeProjectXYZRGBDPoseOnly *edge = new EdgeProjectXYZRGBDPoseOnly(
-        Eigen::Vector3d(pts2[i].x, pts2[i].y, pts2[i].z));
-    edge->setVertex(0, pose);
-    edge->setMeasurement(Eigen::Vector3d(pts1[i].x, pts1[i].y, pts1[i].z));
-    edge->setInformation(Eigen::Matrix3d::Identity());
-    optimizer.addEdge(edge);
-  }
-
-  chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-  optimizer.initializeOptimization();
-  optimizer.optimize(10);
-  chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-  chrono::duration<double> time_used =
-      chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-  cout << "optimization costs time: " << time_used.count() << " seconds."
-       << endl;
-
-  cout << endl << "after optimization:" << endl;
-  cout << "T=\n" << pose->estimate().matrix() << endl;
-
-  // convert to cv::Mat
-  Eigen::Matrix3d R_ = pose->estimate().rotationMatrix();
-  Eigen::Vector3d t_ = pose->estimate().translation();
-  R = (Mat_<double>(3, 3) << R_(0, 0), R_(0, 1), R_(0, 2), R_(1, 0), R_(1, 1),
-       R_(1, 2), R_(2, 0), R_(2, 1), R_(2, 2));
-  t = (Mat_<double>(3, 1) << t_(0, 0), t_(1, 0), t_(2, 0));
-}
